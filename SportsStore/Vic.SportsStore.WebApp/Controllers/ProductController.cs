6d963a8cfe9a426cd8eb5214ab329decdde1fd9f
JW <JW>
2020-08-02 13:09:46 -0400

    6[NEW] Add NavController

--- SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -56,7 +56,10 @@ namespace Vic.SportsStore.WebApp.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = ProductsRepository.Products.Count()
+                    TotalItems = ProductsRepository
+                    .Products
+                    .Where(p => category == null || p.Category == category)
+                    .Count()
                 },
                 CurrentCategory = category
             };
