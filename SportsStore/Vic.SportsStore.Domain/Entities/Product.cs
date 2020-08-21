2c47b5bd0ef0d6a6d1758838818423775175ea5f
JW <JW>
2020-08-20 20:04:04 -0400

    4[CHG]add img

--- SportsStore/Vic.SportsStore.Domain/Entities/Product.cs
+++ SportsStore/Vic.SportsStore.Domain/Entities/Product.cs
@@ -20,7 +20,11 @@ namespace Vic.SportsStore.Domain.Entities
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
+
         [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
---
+++ SportsStore/Vic.SportsStore.WebApp/Infrastructure/Concret/DbAuthProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using Vic.SportsStore.WebApp.Infrastructure.Abstract;
+
+namespace Vic.SportsStore.WebApp.Infrastructure.Concret
+{
+    public class DbAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = false;
+            if (username.Equals("admin", StringComparison.OrdinalIgnoreCase)
+                && password.Equals("adminpwd", StringComparison.OrdinalIgnoreCase))
+            {
+                result = true;
+            }
+
+
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        }
+    }
+}
